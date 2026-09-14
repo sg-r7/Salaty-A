2b9e136c47766cbf2174dc920cf06dcec98e5dd7
Mohammed Molhim Dahi <4sgq.rr@gmail.com>
2026-09-14 23:35:28 +0300

    Create RootLayout component with ThemeProvider

---
+++ app/_layout.tsx
@@ -0,0 +1,15 @@
+import "../global.css";
+import { Stack } from "expo-router";
+import { StatusBar } from "expo-status-bar";
+import { ThemeProvider } from "@/lib/theme-provider";
+import "@/lib/notifications";
+
+export default function RootLayout() {
+  return (
+    <ThemeProvider>
+      <StatusBar style="auto" />
+      <Stack screenOptions={{ headerShown: false }} />
+    </ThemeProvider>
+  );
+}
+
