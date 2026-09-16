7a1b5dd1f7b6c4cbfc72150efbd9267a5f4bf075
Mohammed Molhim Dahi <4sgq.rr@gmail.com>
2026-09-16 23:43:50 +0300

    Implement app update check in RootLayout
    Add useEffect to check for app updates on layout mount.

--- app/_layout.tsx
+++ app/_layout.tsx
@@ -1,10 +1,16 @@
+import { useEffect } from "react";
 import { Stack } from "expo-router";
 import { StatusBar } from "expo-status-bar";
 import { ThemeProvider } from "../lib/theme-provider";
 import { PrayerProvider } from "../src/context/PrayerContext";
+import { checkAppUpdate } from "../src/services/updateService";
 import "../lib/notifications";
 
 export default function RootLayout() {
+  useEffect(() => {
+    checkAppUpdate();
+  }, []);
+
   return (
     <ThemeProvider>
       <PrayerProvider>
